06bf6c91e8c5129a1de0ee6c17d5754177336c50
simon0820s <simon0820s@gmail.com>
2023-11-16 19:58:52 -0500

    react Router

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,3 +1,4 @@
+import { useRoutes, BrowserRouter } from 'react-router-dom'
 import Home from '../Home'
 import MyAccount from '../MyAccount'
 import MyOrder from '../MyOrder'
@@ -6,16 +7,26 @@ import NotFound from '../NotFound'
 import SignIn from '../SignIn'
 import "./App.css"
 
+const AppRoutes = () => {
+
+  let routes = useRoutes([
+    { path: '/', element: <Home /> },
+    { path: '/MyAccount', element: <MyAccount /> },
+    { path: '/MyOrder', element: <MyOrder /> },
+    { path: '/MyOrders', element: <MyOrders /> },
+    { path: '/Not Found', element: <NotFound /> },
+    { path: '/SignIn', element: <SignIn /> },
+  ])
+
+  return routes
+}
+
 const App = () => {
+
   return (
-    <div className="bg-red-500">
-      <Home />
-      <MyAccount />
-      <MyOrder />
-      <MyOrders />
-      <NotFound />
-      <SignIn />
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   )
 }
 
