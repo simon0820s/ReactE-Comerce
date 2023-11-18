71e244044c67150f2b73fa55a16e788b93f3ac4c
simon0820s <simon0820s@gmail.com>
2023-11-17 20:11:38 -0500

    create NavBarg

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,4 +1,5 @@
 import { useRoutes, BrowserRouter } from 'react-router-dom'
+import NavBar from '../../Components/NavBar'
 import Home from '../Home'
 import MyAccount from '../MyAccount'
 import MyOrder from '../MyOrder'
@@ -11,11 +12,11 @@ const AppRoutes = () => {
 
   let routes = useRoutes([
     { path: '/', element: <Home /> },
-    { path: '/MyAccount', element: <MyAccount /> },
-    { path: '/MyOrder', element: <MyOrder /> },
-    { path: '/MyOrders', element: <MyOrders /> },
-    { path: '/Not Found', element: <NotFound /> },
-    { path: '/SignIn', element: <SignIn /> },
+    { path: '/my-account', element: <MyAccount /> },
+    { path: '/my-order', element: <MyOrder /> },
+    { path: '/my-orders', element: <MyOrders /> },
+    { path: '/sign-in', element: <SignIn /> },
+    { path: '/*', element: <NotFound /> },
   ])
 
   return routes
@@ -25,7 +26,8 @@ const App = () => {
 
   return (
     <BrowserRouter>
-      <AppRoutes />
+      <NavBar />
+      <AppRoutes /> 
     </BrowserRouter>
   )
 }
