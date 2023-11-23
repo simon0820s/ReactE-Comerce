369b626dd2872ffcc97bddc105a81d2ea4a2a8a6
simon0820s <simon0820s@gmail.com>
2023-11-23 09:27:23 -0500

    shown product selected on product detail

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -1,6 +1,6 @@
 import XMarkIcon from '@heroicons/react/24/solid/XMarkIcon'
 import { useContext } from 'react'
-import {ShoppingCartContext} from '../../Context'
+import { ShoppingCartContext } from '../../Context'
 import './styles.css'
 
 const ProductDetail = () => {
@@ -15,10 +15,21 @@ const ProductDetail = () => {
         <h2 className='font-medium text-xl'>Detail</h2>
         <div>
           <XMarkIcon
-           className='h-6 w-6 text-black cursor-pointer'
-           onClick={() =>  context.closeProductDetail()} />
+            className='h-6 w-6 text-black cursor-pointer'
+            onClick={() => context.closeProductDetail()} />
         </div>
       </div>
+      <figure className='px-6'>
+        <img
+          className='w-full h-full rounded-lg'
+          src={context.productToShow.images[0]}
+          alt={context.productToShow.title} />
+      </figure>
+      <p className='flex flex-col p-6'>
+        <span className='font-medium text-2xl mb-1'>${context.productToShow.price}</span>
+        <span className='font-medium text-xl'>{context.productToShow.title}</span>
+        <span className='font-light text-sm'>{context.productToShow.description}</span>
+      </p>
     </aside >
   )
 }
