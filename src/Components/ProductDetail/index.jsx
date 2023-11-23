48fe260b0e515553f16b425f43cbc750ff564462
simon0820s <simon0820s@gmail.com>
2023-11-22 22:23:51 -0500

    hidde and show product detail

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -1,16 +1,24 @@
 import XMarkIcon from '@heroicons/react/24/solid/XMarkIcon'
+import { useContext } from 'react'
+import {ShoppingCartContext} from '../../Context'
 import './styles.css'
 
 const ProductDetail = () => {
+
+  const context = useContext(ShoppingCartContext)
+
+  console.log(context.isOpenProductDetail)
+
   return (
-    <aside className="product-detail flex flex-col fixed right-0 border-black rounded-lg bg-white">
+    <aside
+      className={`${context.isOpenProductDetail ? 'flex' : 'hidden'} product-detail flex flex-col fixed right-0 border-black rounded-lg bg-white`}>
       <div className='flex justify-between items-center p-6'>
         <h2 className='font-medium text-xl'>Detail</h2>
         <div>
           <XMarkIcon className='h-6 w-6 text-black' />
         </div>
       </div>
-    </aside>
+    </aside >
   )
 }
 
