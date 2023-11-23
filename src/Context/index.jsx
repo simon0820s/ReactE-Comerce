87e9def91fc9cbc394aaeaf1fa1d08193955d84e
simon0820s <simon0820s@gmail.com>
2023-11-22 22:08:24 -0500

    create open detail global state

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -4,11 +4,17 @@ export const ShoppingCartContext = createContext()
 
 export const ShoppingCartProvider = ({ children }) => {
   const [count, setCount] = useState(0)
+  const [isOpenProductDetail, setIsOpenProductDetail] = useState(false);
 
+  const openProductDetail = () => setIsOpenProductDetail(true);
+  const closeProductDetail = () => setIsOpenProductDetail(false);
   return (
     <ShoppingCartContext.Provider value={{
       count,
-      setCount
+      setCount,
+      openProductDetail,
+      closeProductDetail,
+      isOpenProductDetail
     }}>
       {children}
     </ShoppingCartContext.Provider>
